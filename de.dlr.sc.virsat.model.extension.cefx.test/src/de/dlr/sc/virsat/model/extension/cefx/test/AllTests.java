ad5a1d3f36e02a7cf9b8d337c5a1ce81c2ca3ea9
kova_an <andrii.kovalov@dlr.de>
2019-08-27 18:31:45 +0200

    Added CefxHierarchyLevelChecker tests (Task #32)
    [skip ci]

--- de.dlr.sc.virsat.model.extension.cefx.test/src/de/dlr/sc/virsat/model/extension/cefx/test/AllTests.java
+++ de.dlr.sc.virsat.model.extension.cefx.test/src/de/dlr/sc/virsat/model/extension/cefx/test/AllTests.java
@@ -19,6 +19,7 @@ import de.dlr.sc.virsat.model.extension.cefx.calculation.modes.ModeVectorResultT
 import de.dlr.sc.virsat.model.extension.cefx.calculation.modes.ParameterGetterTest;
 import de.dlr.sc.virsat.model.extension.cefx.calculation.modes.ParameterSetterTest;
 import de.dlr.sc.virsat.model.extension.cefx.excel.ExcelUpdaterTest;
+import de.dlr.sc.virsat.model.extension.cefx.hierarchy.CefxHierarchyLevelCheckerTest;
 import de.dlr.sc.virsat.model.extension.cefx.util.CefModeHelperTest;
 import de.dlr.sc.virsat.model.extension.cefx.validator.ValidatorTest;
 import junit.framework.JUnit4TestAdapter;
@@ -38,6 +39,7 @@ import junit.framework.JUnit4TestAdapter;
 	ModeVectorResultTest.class,
 	ParameterGetterTest.class,
 	ParameterSetterTest.class,
+	CefxHierarchyLevelCheckerTest.class
 	})
 
 public class AllTests {
