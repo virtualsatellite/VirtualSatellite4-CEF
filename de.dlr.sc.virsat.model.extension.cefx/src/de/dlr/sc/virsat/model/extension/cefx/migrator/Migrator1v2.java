6eee3f433766b1bf031e2836c4fb0b6a5d2a6e1d
ngat_di <ngat_di@SC-010199.intra.dlr.de>
2024-03-14 14:53:35 +0100

    Task #193 updating the Migrator's class.

--- de.dlr.sc.virsat.model.extension.cefx/src/de/dlr/sc/virsat/model/extension/cefx/migrator/Migrator1v2.java
+++ de.dlr.sc.virsat.model.extension.cefx/src/de/dlr/sc/virsat/model/extension/cefx/migrator/Migrator1v2.java
@@ -9,7 +9,6 @@
  *******************************************************************************/
 package de.dlr.sc.virsat.model.extension.cefx.migrator;
 
-import de.dlr.sc.virsat.model.dvlm.concepts.Concept;
 import de.dlr.sc.virsat.model.concept.migrator.IMigrator;
 
 
