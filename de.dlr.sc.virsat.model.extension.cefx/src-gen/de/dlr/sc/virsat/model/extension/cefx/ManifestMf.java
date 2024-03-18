e38ef8903639637e26c14747e7a32966b2ed0cde
Dennis Eller <95470420+dellerDLR@users.noreply.github.com>
2024-03-18 15:12:02 +0100

    Fix wrong copyright message

--- de.dlr.sc.virsat.model.extension.cefx/src-gen/de/dlr/sc/virsat/model/extension/cefx/ManifestMf.java
+++ de.dlr.sc.virsat.model.extension.cefx/src-gen/de/dlr/sc/virsat/model/extension/cefx/ManifestMf.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008-2019 German Aerospace Center (DLR), Simulation and Software Technology, Germany.
+ * Copyright (c) 2008-2024 German Aerospace Center (DLR), Institute for Software Technology, Germany.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
